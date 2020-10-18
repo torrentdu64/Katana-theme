938fa44650a10afb0497b6591f8b1101dcae043a
torrent benoit <torrentdu64@gmail.com>
2020-10-18 15:25:08 +1300

    banner function and css component

--- functions.php
+++ functions.php
@@ -17,3 +17,36 @@ function meta_info(){
 }
 
 add_action('after_setup_theme', 'meta_info');
+
+
+function pageBanner($args = NULL) {
+
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+
+  if (!$args['subtitle']) {
+    // $args['subtitle'] = get_field('page_banner_subtitle');
+    $args['subtitle'] = 'default value';
+  }
+
+  if (!$args['photo']) {
+    // if (get_field('page_banner_background_image')) {
+    //   $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    // } else {
+    //   $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    // }
+    $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+  }
+
+  ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>
+  </div>
+<?php }
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 
 <?php  get_header(); ?>
 <br>
+<?php pageBanner(array(
+      'title' => 'my title',
+      'subtitle' => 'custome totdo'
+
+)); ?>
 <h2>theme</h2>
 <br>
 <div>
